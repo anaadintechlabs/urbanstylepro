d94e71d362138fbf5d4ad77e31806a3fc71cecb7
sanchit khurana <sanchit.khurana@dawnbit.com>
2020-01-08 01:09:38 +0530

    category meta

---
+++ Backend/product/src/main/java/com/urbanstyle/product/controller/CategoryMetaController.java
@@ -0,0 +1,31 @@
+package com.urbanstyle.product.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.urbanstyle.product.util.CommonResponseSender;
+@RestController
+@RequestMapping("/meta")
+@CrossOrigin(origins="*")
+public class CategoryMetaController {
+
+	@RequestMapping(value="/getAllMetaOfCategory",method=RequestMethod.GET)
+	public Map<String,Object> getAllMetaOfCategory(
+			@RequestParam(value="categoryId") long categoryId,
+			HttpServletRequest request,HttpServletResponse response)
+	{
+		final HashMap<String, Object> map = new HashMap<>();
+		map.put("metaList", categoryVariationService.getAllVariationOfCategory(categoryId));
+		return CommonResponseSender.createdSuccessResponse(map, response);
+		
+	}
+}
