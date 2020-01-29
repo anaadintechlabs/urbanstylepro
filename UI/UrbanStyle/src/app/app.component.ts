393280c3485b820be2db688f4a75a3a6f098073a
Ankit <ankit@anaadigsoft.com>
2020-01-30 01:40:47 +0530

    inventory

--- UI/UrbanStyle/src/app/app.component.ts
+++ UI/UrbanStyle/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Router, NavigationStart } from '@angular/router';
 
+export let Refresh = false;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'UrbanStyle';
+  subscription : Subscription
+
+  constructor(
+    private _router : Router
+  ){
+  this.subscription = this._router.events.subscribe(event=>{
+      if(event instanceof NavigationStart){
+        Refresh = !this._router.navigated;
+      }
+    })
+  }
 }
