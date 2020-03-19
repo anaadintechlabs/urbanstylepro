1bd2e8033845ecae74ea209c1f4809e3293a9a08
paras.miglani <parasmiglani1025@gmail.com>
2020-03-19 08:39:59 +0530

    getsales of order 1
    Change-Id: I46d9b6dd5c0cc4f96c1744a2add053a99554a9d5
    Signed-off-by: paras.miglani <parasmiglani1025@gmail.com>

--- Backend/order/src/main/java/com/urbanstyle/order/Repository/PaymentWalletTransactionRepo.java
+++ Backend/order/src/main/java/com/urbanstyle/order/Repository/PaymentWalletTransactionRepo.java
@@ -1,5 +1,8 @@
 package com.urbanstyle.order.Repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +12,8 @@ import com.anaadihsoft.common.master.PaymentWalletTransaction;
 public interface PaymentWalletTransactionRepo extends PagingAndSortingRepository<PaymentWalletTransaction, Long> {
 
 	PaymentWalletTransaction findByRecieverAndOrderId(String string, long id);
+
+	@Query("FROM PaymentWalletTransaction where orderProds =?1 and status =?2 order by amount")
+	List<PaymentWalletTransaction> getTransactionofOrder(long orderProdId,String status);
 
 }
